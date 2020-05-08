550b79b470a9afeca3a5f11ad970319a7eabad49
Cam Appel <c.appel@columbia.edu>
2020-05-07 22:59:41 -0400

    Add files via upload

---
+++ london_visual/london_visual/js/search.js
@@ -0,0 +1,16 @@
+// Creating map object
+var myMap = L.map("map", {
+    center: [34.0522, -118.2437],
+    zoom: 8
+});
+
+// Adding tile layer
+
+var layer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20,
+    minZoom: 0
+});
+
+layer.addTo(myMap);
