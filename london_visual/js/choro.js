7c7642a7b7418bac6ad42535f54a5274e8495660
Cam Appel <c.appel@columbia.edu>
2020-05-09 21:06:55 -0400

    Update choro.js

--- london_visual/js/choro.js
+++ london_visual/js/choro.js
@@ -11,7 +11,7 @@ var overlays;
 
 // Adding tile layer
 
-var layer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), {
+var layer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), {
    attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
    subdomains: 'abcd',
    maxZoom: 20,
