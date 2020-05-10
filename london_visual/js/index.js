b1622d904a69a99f856436ac7e39cbd88001c6a5
Cam Appel <c.appel@columbia.edu>
2020-05-09 20:56:41 -0400

    Update index.js

--- london_visual/js/index.js
+++ london_visual/js/index.js
@@ -6,7 +6,7 @@ var map = L.map('map', {
 });
 
 // Tile layer
-var layer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), {
+var layer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 20,
